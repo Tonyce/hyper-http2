93eabaf0bcef6b42e2852e9c1069b375529ac3c2
d_ttang <d_ttang@163.com>
2021-04-05 19:02:48 +0800

    some

--- src/bin/h2_proxy.rs
+++ src/bin/h2_proxy.rs
@@ -37,29 +37,26 @@ async fn main() {
     let in_addr = ([127, 0, 0, 1], 3001).into();
     let out_addr: SocketAddr = ([127, 0, 0, 1], 50051).into();
 
+    let out_addr_clone = out_addr.clone();
+    let client_main = Client::builder()
+        // .pool_idle_timeout(Duration::from_secs(30))
+        .http2_only(true)
+        .build_http::<Body>();
     // The closure inside `make_service_fn` is run for each connection,
     // creating a 'service' to handle requests for that specific connection.
-    // let make_service = make_service_fn(move |_| async {
-    //     let client = client_main.clone();
+    let make_service = make_service_fn(|_| async move {
+        let client = client_main.clone();
 
-    //     // This is the `Service` that will handle the connection.
-    //     // `service_fn` is a helper to convert a function that
-    //     // returns a Response into a `Service`.
-    //     Ok::<_, Infallible>(service_fn(|mut req| async move {
-    //         let uri_string = format!(
-    //             "http://{}{}",
-    //             out_addr_clone,
-    //             req.uri()
-    //                 .path_and_query()
-    //                 .map(|x| x.as_str())
-    //                 .unwrap_or("/")
-    //         );
-    //         let uri = uri_string.parse().unwrap();
-    //         *req.uri_mut() = uri;
-    //         // let res = client.request(req).await;
-    //         Ok(Response::new(Body::from("Hello, World!")))
-    //     }))
-    // });
+        // let remote_addr = socket.remote_addr();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
+                Ok::<_, Infallible>(Response::new(Body::from(format!(
+                    "Hello, {}!",
+                    "remote_addr"
+                ))))
+            }))
+        }
+    });
 
     let make_service = make_service_fn(|_conn| {
         // This is the `Service` that will handle the connection.
